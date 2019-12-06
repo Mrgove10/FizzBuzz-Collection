34b6b45b81dd2576912f328b9633d7a5561f581c
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-06 10:21:55 +0100

    added go

---
+++ GoLang/FizzBuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+	limit := 100
+	sum := 0
+	for i := 0; i < limit; i++ {
+		string := ""
+		if(i % 3 == 0){
+			string += "Fizz"
+		}
+		if(i % 5 == 0){
+			string += "Buzz"
+		}
+		if (i % 5 != 0 && i % 3 != 0) {
+			string += strconv.Itoa(i);
+		}
+		fmt.Println(string)
+	}
+	fmt.Println(sum)
+}
